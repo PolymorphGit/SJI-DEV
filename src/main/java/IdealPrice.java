5efa5014352323be2884ed481b465ce3ba89647a
iyingac <iyingac@gmail.com>
2017-01-25 10:27:09 +0700

    Restructure Object

--- src/main/java/IdealPrice.java
+++ src/main/java/IdealPrice.java
@@ -55,7 +55,7 @@ class IdealPrice
 		for(NPD npd : DictNPD.values())
 		{
 			output += "NPD Id: " + npd.Id + "<br/>";
-			output += "FG:" + npd.Debug + "<br/>"; 
+			output += "Debug: " + npd.Debug + "<br/>"; 
 			/*
 			for(FG fg : npd.listFG)
 			{
